d1cb6df6f0ac1811a2e4c49d4926bb1cd85f55ff
Jason Steelman <jasonsteelmancoder@gmail.com>
2024-08-28 08:33:56 -0400

    filter images by presence of humans

--- person_finder.py
+++ person_finder.py
@@ -5,7 +5,7 @@ import shutil
 
 load_dotenv()
 
-archive_path = f'C:/Users/{os.getenv('USERNAME')}/Pictures/ath_photo_archive/Athens Photo Archive Files'
+archive_path = f"C:/Users/{os.getenv('USERNAME')}/Pictures/ath_photo_archive/Athens Photo Archive Files"
 
 for root, dirs, files in os.walk(archive_path):
     for file in files:
@@ -17,7 +17,7 @@ for root, dirs, files in os.walk(archive_path):
             (humans, _) = hog.detectMultiScale(image, winStride=(10, 10), padding=(32, 32), scale=1.1)
             if len(humans) > 0:
                 source_path = os.path.join(root, file)
-                destination_folder = f"C:/Users/{os.getenv("USERNAME")}/Pictures/archive_of_people"
+                destination_folder = f"C:/Users/{os.getenv('USERNAME')}/Pictures/archive_of_people"
                 destination_path = os.path.join(destination_folder, os.path.basename(source_path))
                 shutil.copy(source_path, destination_path)
                 print("copied!")
